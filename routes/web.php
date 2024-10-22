a0f04ba1d948d63a5c12ca2e425fcf652774d7f0
Icaro_K <ikkikaminski@hotmail.com>
2024-10-22 14:37:42 -0300

    1.0.1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SobreNosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", [HomeController::class,"home"])->name('site.index');
+
+Route::get('/sobre-nos', [SobreNosController::class,'sobrenos'])->name('site.sobrenos');
+
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+
+Route::get('/login', function(){
+    return('login');
+})->name('site.login');
+
+
+
+Route::prefix('/app')->group(function(){
+    
+    Route::get('/clientes', function(){
+        return('clientes');
+    })->name('app.clientes');
+    
+    Route::get('/fornecedores', function(){
+        return('fornecedores');
+    })->name('app.fornecedores');
+    
+    Route::get('/produtos', function(){
+        return('produtos');
+    })->name('app.produtos');
 });
+
+Route::get('/rota1', function(){
+    echo 'rota1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+
+// Route::redirect('/rota2', '/rota1');
